172e973b7ec9e3062f7f26efe8feeb5f31e8e6f9
cemil fahreci <cemilfahreci@icloud.com>
2025-04-08 22:01:59 +0300

    Feature - #302: Created Mage character with castSpell() Feature - #303: Created Warrior character with attack()

---
+++ Week6/EldoriaGame/src/main/java/org/example/Mage.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Mage extends Hero implements Questable {
+
+    public Mage(String name, int level) {
+        super(name, level);
+    }
+
+    public void castSpell() {
+        System.out.println(getName() + " casts a powerful spell!");
+    }
+
+    @Override
+    public void acceptQuest(String questName) {
+        System.out.println(getName() + " the Mage accepts the quest: " + questName);
+    }
+}
